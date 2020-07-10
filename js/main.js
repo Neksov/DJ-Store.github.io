29e67e2192f7f7b8ee8f60942bb5d395132b739a
Neksov <n2v@list.ru>
2020-07-10 12:51:39 +0300

    mobile menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   $('.menu__burger').click(function (event) {
-    $('.menu__burger, .menu__list, .menu__mail, .menu__btn').toggleClass('active');
+    $('.menu__burger, .menu__list, .menu__mail, .menu__btn, .nav__item').toggleClass('active');
     $('body').toggleClass('lock');
+  });
+  $('.nav__item').click(function (event) {
+    $('.menu__list, .menu__burger, .menu__mail, .menu__btn').removeClass("active");
   });
 });
