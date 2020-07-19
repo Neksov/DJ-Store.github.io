5020932eb14565537c4a2999fbcfa9c505d7e702
Neksov <n2v@list.ru>
2020-07-19 21:30:12 +0300

    new ficha

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 //бургер меню
 $(document).ready(function () {
-  $('.menu__burger').click(function (event) {
-    $('.menu__burger, .menu__list, .menu__mail, .menu__btn, .nav__item').toggleClass('active');
-    $('body').toggleClass('lock');
+  $(".menu__burger").click(function (event) {
+    $(
+      ".menu__burger, .menu__list, .menu__mail, .menu__btn, .nav__item"
+    ).toggleClass("active");
+    $("body").toggleClass("lock");
   });
-  $('.nav__item').click(function (event) {
-    $('.menu__list, .menu__burger, .menu__mail, .menu__btn').removeClass("active");
+  $(".nav__item").click(function (event) {
+    $(".menu__list, .menu__burger, .menu__mail, .menu__btn").removeClass(
+      "active"
+    );
   });
-});
-//слайдер 
-var swiper = new Swiper('.swiper-container', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  //слайдер
+  var swiper = new Swiper(".swiper-container", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
 });
